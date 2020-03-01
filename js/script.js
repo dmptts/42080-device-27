093531a949060e77429f8aa88fa9b1ce2a50a4b1
Dmitriy Solovev <dmptts@gmail.com>
2020-03-01 18:34:13 +0300

    Добавляет на главную блок с сервисами

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var servicesControls = document.querySelector(".services-controls");
+servicesControls.classList.remove("visually-hidden");
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+var slideIndex = 1;
+var promoSlide = "promoSlide";
+var servicesItem = "servicesItem";
+
+showSlides(slideIndex, promoSlide);
+showSlides(slideIndex, servicesItem);
+
+function currentSlide(n, x) {
+  showSlides(slideIndex = n, x);
 };
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("promo-slide");
-  var controls = document.getElementsByClassName("promo-slider-control")
+function showSlides(n, x) {
+  var slides;
+  var controls;
 
-  for (i = 0; i < slides.length; i++) {
+  if (x == "promoSlide") {
+    slides = document.getElementsByClassName("promo-slide");
+    controls = document.getElementsByClassName("promo-slider-control");
+  } else {
+    slides = document.getElementsByClassName("services-item");
+    controls = document.getElementsByClassName("services-control");
+  };
+
+  for (var i = 0; i < slides.length; i++) {
     slides[i].classList.add("visually-hidden");
-    controls[i].classList.remove("promo-slider-control-active")
+    controls[i].classList.remove("active")
   };
 
   slides[slideIndex - 1].classList.remove("visually-hidden");
-  controls[slideIndex - 1].classList.add("promo-slider-control-active");
-}
+  controls[slideIndex - 1].classList.add("active");
+};
