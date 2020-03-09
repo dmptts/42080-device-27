347aedfdc62bd34cf85645b8030b636c39926ae2
Dmitriy Solovev <dmptts@gmail.com>
2020-03-09 13:25:23 +0300

    Реализует показ/сокрытие модальных окон

--- js/script.js
+++ js/script.js
@@ -32,3 +32,42 @@ function showSlides(n, x) {
   slides[slideIndex - 1].classList.remove("visually-hidden");
   controls[slideIndex - 1].classList.add("active");
 };
+
+var feedbackBtn = document.querySelector(".feedback-btn");
+var map = document.querySelector(".map");
+var mapOverlay = document.querySelector(".map-overlay");
+var modalFeedbackCloseBtn = document.querySelector(".modal-feedback-btn-close");
+var modalMapCloseBtn = document.querySelector(".modal-map-btn-close");
+var modalFeedback = document.querySelector(".modal-feedback");
+var modalMap = document.querySelector(".modal-map");
+var modalOverlay = document.querySelector(".modal-overlay");
+
+feedbackBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalFeedback.classList.add("modal-show");
+  modalOverlay.classList.add("modal-show");
+});
+
+mapOverlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalMap.classList.add("modal-show");
+  modalOverlay.classList.add("modal-show");
+});
+
+modalOverlay.addEventListener("click", function (evt) {
+  modalOverlay.classList.remove("modal-show");
+  modalFeedback.classList.remove("modal-show");
+  modalMap.classList.remove("modal-show");
+});
+
+modalFeedbackCloseBtn.addEventListener("click", function (evt) {
+  modalOverlay.classList.remove("modal-show");
+  modalFeedback.classList.remove("modal-show");
+});
+
+modalMapCloseBtn.addEventListener("click", function (evt) {
+  modalOverlay.classList.remove("modal-show");
+  modalMap.classList.remove("modal-show");
+});
+
+
